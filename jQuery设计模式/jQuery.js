b54cd26fecde12a64201be2609181f09bf78e714
Kakaken <wdnmd3313@gmail.com>
2022-05-13 18:57:12 +0800

    jquery设计模式

---
+++ jQuery设计模式/jQuery.js
@@ -0,0 +1,38 @@
+window.$ = window.jQuery = function(selectorOrArray) {
+    let elements;
+    if (typeof selectorOrArray === "string") {
+        elements = document.querySelectorAll(selectorOrArray);
+    } else if (selectorOrArray instanceof Array) {
+      elements = selectorOrArray;
+    }
+
+    const api = {
+        __proto__: jQuery.prototype,  
+        elements: elements, 
+        oldApi:selectorOrArray.oldApi}
+    return api
+  };
+  
+  jQuery.fn = jQuery.prototype = {
+    constructor: jQuery,
+   find(selector) {
+    let array = [];
+    for (let i = 0; i < this.elements.length; i++) {
+      const elements2 = Array.from(this.elements[i].querySelectorAll(selector));
+      array = array.concat(elements2);
+    }
+    oldApi = this; 
+    return jQuery(array);
+    },
+    addClass(className) {
+      for (let i = 0; i < this.elements.length; i++) {
+        const element = this.elements[i];
+        element.classList.add(className);
+      }
+      return this;
+    },
+    end() {
+      return this.oldApi; 
+    }
+  };
+
---
+++ jQuery设计模式/main.js
@@ -0,0 +1,12 @@
+console.log('main.js已启动')
+
+
+
+const api1 = jQuery('#test') // 等于const api1 = $('#test')   #是ID .是class
+api1.addClass('数组添加')
+
+
+api1.find('.child').addClass('shit')
+api1.end();
+api1.addClass('wuhu')
+$('#test').addClass('happy')
